461cb9de1557236e291c0ff73816731caa5ab02e
worknehedimealem <131806928+worknehedimealem@users.noreply.github.com>
2025-07-01 09:48:03 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function appendValue(val) {
+  document.getElementById("display").value += val;
+}
+
+function clearDisplay() {
+  document.getElementById("display").value = '';
+}
+
+function calculate() {
+  try {
+    document.getElementById("display").value = eval(document.getElementById("display").value);
+  } catch {
+    document.getElementById("display").value = 'Error';
+  }
+}
